80cf85113b02cd3ba01ad2ffa2c9a666875d8e21
borg <bruno.riguetto@gft.com>
2022-10-24 21:03:55 -0300

    :tada: Commit inicial

---
+++ src/main/java/br/gft/lirics/LiricsApplication.java
@@ -0,0 +1,13 @@
+package br.gft.lirics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gft/lirics/LiricsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gft.lirics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
